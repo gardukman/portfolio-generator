5f3914b8c8fc5a27a6c6d94ac3276f4050153fa2
Duke Nukem <garrettduke87@gmail.com>
2020-09-25 00:07:28 -0600

    finished generate-webpage

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello Node!');
+const fs = require('fs');
+// with this statement, the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
